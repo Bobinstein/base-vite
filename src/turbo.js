c5a15194460947427d680ddfafb26ed62e7cc0f2
bobinstein <cavanaugh.stephen1991@gmail.com>
2025-05-13 15:39:44 -0400

    chore: update

--- src/turbo.js
+++ src/turbo.js
@@ -1,22 +1,22 @@
-import { TurboFactory } from '@ardrive/turbo-sdk/web'
+import { TurboFactory } from "@ardrive/turbo-sdk/web";
 
+export default async function doTheTurbo() {
+  console.log(import.meta.env.VITE_JWK);
 
+  const jwk = JSON.parse(import.meta.env.VITE_JWK);
 
-const jwk = "replace with jwk"
-
-export default async function doTheTurbo(){
   const turbo = TurboFactory.authenticated({
-    privateKey: jwk
-  })
+    privateKey: jwk,
+  });
 
-  console.log('turbo: ', turbo)
-  const currencies = await turbo.getSupportedCurrencies()
-  console.log(currencies)
+  console.log("turbo: ", turbo);
+  const currencies = await turbo.getSupportedCurrencies();
+  console.log(currencies);
 
   const approvals = await turbo.getCreditShareApprovals({
-    userAddress: "cF0H0SKdnaDTqWKY9iJKBktTpdEWgb3GnlndE7ABv0Q"
-  })
-  console.log(approvals)
+    userAddress: "cF0H0SKdnaDTqWKY9iJKBktTpdEWgb3GnlndE7ABv0Q",
+  });
+  console.log(approvals);
 
   // const reply = await turbo.shareCredits({
   //   approvedAddress: 'cF0H0SKdnaDTqWKY9iJKBktTpdEWgb3GnlndE7ABv0Q',
@@ -25,6 +25,20 @@ export default async function doTheTurbo(){
   // })
   // console.log(reply)
 
-  const balance = await turbo.getBalance()
-  console.log(balance)
+  const balance = await turbo.getBalance();
+  console.log(balance);
+
+  const blob = new Blob(["Hello, world!"], { type: "text/plain" });
+
+  // Get the ReadableStream
+  const readableStream = blob.stream();
+
+  // Use the stream
+  const reader = readableStream.getReader();
+
+  reader.read().then(({ value, done }) => {
+    if (!done) {
+      console.log("Streamed data:", new TextDecoder().decode(value)); // Logs "Hello, world!"
+    }
+  });
 }
